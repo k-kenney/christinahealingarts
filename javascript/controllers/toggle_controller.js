399a0b3fe86984803bd4ad3b6dbba6f1d25208af
Kayla Kenney <kaylachristinekenney@gmail.com>
2023-06-19 09:07:42 +0200

    working on media queries for different browsers

--- javascript/controllers/toggle_controller.js
+++ javascript/controllers/toggle_controller.js
@@ -2,22 +2,27 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = ["menu", "hamburger", "close"]
-  static values = { isOpen: { type: Boolean, default: false } }
 
-  connect() {
-    console.log("connected!")
-    this.updateMenuVisibility();
-    window.addEventListener("resize", this.updateMenuVisibility.bind(this));
+  static values = { isOpen: Boolean }
+
+  connect = () => {
+    // console.log(this.element)
+    this.updateMenuVisibility()
+    window.addEventListener("resize", this.updateMenuVisibility.bind(this))
   }
 
-  disconnect() {
-    window.removeEventListener("resize", this.updateMenuVisibility.bind(this));
+  disconnect = () => {
+    window.removeEventListener("resize", this.updateMenuVisibility.bind(this))
   }
 
   menu() {
-    console.log("you clicked the button!")
-    console.log(this.menuTarget)
-    this.isOpenValue ? this.hide() : this.show()
+    // console.log("you clicked the button!")
+    // console.log(this.menuTarget)
+    if (this.isOpenValue) {
+      this.hide()
+    } else {
+      this.show()
+    }
     this.isOpenValue = !this.isOpenValue
   }
 
@@ -35,48 +40,9 @@ export default class extends Controller {
 
   updateMenuVisibility() {
     if (window.innerWidth <= 992) {
-      this.hide();
+      this.hide()
     } else {
-      this.show();
+      this.show()
     }
   }
 }
-
-// import { Controller } from "@hotwired/stimulus"
-
-// export default class extends Controller {
-//   static targets = ["menu", "hamburger", "close"]
-//   static values = { isOpen: { type: Boolean, default: false } }
-
-//   // initialize() {
-//   //   this.isOpen = false
-//   // }
-
-//   connect() {
-//     console.log("connected!")
-//     if (window.innerWidth <= 768) {
-//       this.hide()
-//     } else {
-//       this.show()
-//     }
-//   }
-
-//   menu() {
-//     console.log("you clicked the button!")
-//     console.log(this.menuTarget)
-//     this.isOpenValue ? this.hide() : this.show()
-//     this.isOpenValue = !this.isOpenValue
-//   }
-
-//   show() {
-//     this.menuTarget.style.display = "block"
-//     this.hamburgerTarget.style.display = "none"
-//     this.closeTarget.style.display = "block"
-//   }
-
-//   hide() {
-//     this.menuTarget.style.display = "none"
-//     this.hamburgerTarget.style.display = "block"
-//     this.closeTarget.style.display = "none"
-//   }
-// }
