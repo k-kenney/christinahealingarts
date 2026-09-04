5d74ed08cc324e285856a0ff2cae467a09dfd97d
Kayla K <105766386+k-kenney@users.noreply.github.com>
2026-09-04 09:33:06 +0200

    streamlined footer and added link updates to facilitators from retreats page

--- javascript/controllers/index.js
+++ javascript/controllers/index.js
@@ -5,9 +5,11 @@ import ToggleController from "./toggle_controller.js";
 import FaqController from "./faq_controller.js";
 import ContactController from "./contact_controller.js";
 import NavbarController from "./navbar_controller.js";
+import FooterController from "./footer_controller";
 
 application.register("hello", HelloController);
 application.register("toggle", ToggleController);
 application.register("faq", FaqController);
 application.register("contact", ContactController);
 application.register("navbar", NavbarController);
+application.register("footer", FooterController);
